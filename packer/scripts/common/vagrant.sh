9d13f2bca13a15f1b805573393c0290164a76cf2
Alan Pinstein <apinstein@mac.com>
2014-03-03 17:24:29 -0500

    Add builder for AWS/EC2 EBS-backed AMI.

--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-mkdir /home/vagrant/.ssh
-wget --no-check-certificate \
-    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
-    -O /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
-chmod -R go-rwsx /home/vagrant/.ssh
+case "$PACKER_BUILDER_TYPE" in 
+
+virtualbox-iso|virtualbox-ovf) 
+    mkdir /home/vagrant/.ssh
+    wget --no-check-certificate \
+        'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+        -O /home/vagrant/.ssh/authorized_keys
+    chown -R vagrant /home/vagrant/.ssh
+    chmod -R go-rwsx /home/vagrant/.ssh
+    ;;
+
+amazon-ebs) 
+    echo "amazon-ebs is a noop for vagrant.sh"
+    ;;
+
+*)
+    echo "Unknown Packer Builder Type >>$PACKER_BUILDER_TYPE<< selected."
+    echo "Known are virtualbox-iso|virtualbox-ovf|amazon-ebs."
+    ;;
+
+esac
+
