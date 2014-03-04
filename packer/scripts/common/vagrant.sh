64f65d573487a324c9a56a88250a315dfa94143c
Alan Pinstein <apinstein@mac.com>
2014-03-03 21:22:30 -0500

    Password-less sudo.

--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -19,7 +19,7 @@ amazon-ebs)
     chmod -R go-rwsx /home/chef/.ssh
     echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCP+jQBBh1hxcoGDB4zkAy+XoR2iFQI8BBwQnHTGGsFOVGCHELUarsql+Uuh/C5jns8JYDYl+kAm5Qc62J117o1Z/E8ZqNg6jvwukBeTwZx+baS4vDx6Ru67Fx6I2q5GQbJ6hmw0Wn0fM9M7PQPC6r55PiMwhGJG/DBbb42c3x2mfIygSPlFHJPn14kJMGVECgqcp3TsnrMZuCCL8Ri9Jr/ge8o/WVkxnjA8hCfyYUfhMNhJWj19irHMxlHPF8nl45D769guLGRdeB0NVhYnqc2AE397u726yEcfCP188hwfAEu0iltGWOhjhpvs/87AsGW4STuhD1hpA8DSOWKoojV" > /home/chef/.ssh/authorized_keys
 
-    grep "^%wheel" /etc/sudoers || echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+    grep "^%wheel" /etc/sudoers || echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     grep "SSH_AUTH_SOCK" /etc/sudoers || echo "Defaults env_keep+=SSH_AUTH_SOCK" >> /etc/sudoers
 
     # only allow key-based, non-root, 'chef' ssh login
