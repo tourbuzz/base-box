a8ef0ac59278be9f2067a1c941f19a607593ace4
Alan Pinstein <apinstein@mac.com>
2014-03-03 21:59:44 -0500

    Do not require tty for sudo; vagrant needs this, maybe chef does too?

--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -21,6 +21,7 @@ amazon-ebs)
 
     grep "^%wheel" /etc/sudoers || echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     grep "SSH_AUTH_SOCK" /etc/sudoers || echo "Defaults env_keep+=SSH_AUTH_SOCK" >> /etc/sudoers
+    echo "Defaults !requiretty" >> /etc/sudoers
 
     # only allow key-based, non-root, 'chef' ssh login
     echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
