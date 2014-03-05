f2eb02be016b5fc52775ab69c79a0c947736e2a9
Alan Pinstein <apinstein@mac.com>
2014-03-05 13:19:44 -0500

    Normalize into just a single "vagrant" user and also leaving requiretty alone.

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
-# only allow key-based, non-root, 'chef' ssh login on port 1471
+# only allow key-based, non-root, 'vagrant' ssh login on port 1471
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "AllowUsers chef" >> /etc/ssh/sshd_config
+echo "AllowUsers vagrant" >> /etc/ssh/sshd_config
 sed -i -e 's/^\#\?Port 22\b/Port 1471/' /etc/ssh/sshd_config
 
 # speed things up
--- packer/scripts/common/vagrant.sh
+++ packer/scripts/common/vagrant.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+echo "Installing public key for vagrant"
+mkdir /home/vagrant/.ssh
+
 case "$PACKER_BUILDER_TYPE" in 
 
 virtualbox-iso|virtualbox-ovf) 
-    mkdir /home/vagrant/.ssh
     wget --no-check-certificate \
         'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
         -O /home/vagrant/.ssh/authorized_keys
-    chown -R vagrant /home/vagrant/.ssh
-    chmod -R go-rwsx /home/vagrant/.ssh
+    ;;
+
+amazon-ebs) 
+    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCP+jQBBh1hxcoGDB4zkAy+XoR2iFQI8BBwQnHTGGsFOVGCHELUarsql+Uuh/C5jns8JYDYl+kAm5Qc62J117o1Z/E8ZqNg6jvwukBeTwZx+baS4vDx6Ru67Fx6I2q5GQbJ6hmw0Wn0fM9M7PQPC6r55PiMwhGJG/DBbb42c3x2mfIygSPlFHJPn14kJMGVECgqcp3TsnrMZuCCL8Ri9Jr/ge8o/WVkxnjA8hCfyYUfhMNhJWj19irHMxlHPF8nl45D769guLGRdeB0NVhYnqc2AE397u726yEcfCP188hwfAEu0iltGWOhjhpvs/87AsGW4STuhD1hpA8DSOWKoojV" \
+        > /home/vagrant/.ssh/authorized_keys
     ;;
 
 *)
@@ -18,3 +23,5 @@ virtualbox-iso|virtualbox-ovf)
 
 esac
 
+chown -R vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
