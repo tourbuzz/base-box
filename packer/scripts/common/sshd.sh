4f26fa9ddb0c6ec84e4c872e1f4ab87586521c52
Alan Pinstein <apinstein@mac.com>
2014-03-05 21:38:33 -0500

    Need to switch iptables port as well.

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -6,6 +6,8 @@ echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "AllowUsers vagrant" >> /etc/ssh/sshd_config
 sed -i -e 's/^\#\?Port 22\b/Port 1471/' /etc/ssh/sshd_config
+sed -i -e 's/^-A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT/-A INPUT -m state --state NEW -m tcp -p tcp --dport 1471 -j ACCEPT/' /etc/sysconfig/iptables
+
 
 # speed things up
 echo "UseDNS no" >> /etc/ssh/sshd_config
