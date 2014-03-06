7560b3facde076b96c222b23cb380f44cb1d380b
Alan Pinstein <apinstein@mac.com>
2014-03-05 22:43:05 -0500

    Disable requiretty for sudoers as vagrant is more tested without pty support.

--- packer/scripts/common/sudoers.sh
+++ packer/scripts/common/sudoers.sh
@@ -2,3 +2,5 @@
 
 grep "^%wheel" /etc/sudoers || echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 grep "SSH_AUTH_SOCK" /etc/sudoers || echo "Defaults env_keep+=SSH_AUTH_SOCK" >> /etc/sudoers
+# vagrant prefers no tty
+echo "Defaults !requiretty" >> /etc/sudoers
